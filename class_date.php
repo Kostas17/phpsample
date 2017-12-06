8a3b1763dd3731a40716ec2e09560f7cdb8e4b94
Kostas17 <34317101+Kostas17@users.noreply.github.com>
2017-12-06 21:02:19 +0200

    Add files via upload

---
+++ class_date.php
@@ -0,0 +1,67 @@
+<?php
+
+class Date
+{
+    protected $day;
+    protected $month;
+    protected $year;
+
+    public function __construct($day,$month,$year)
+    {
+        if (self::validate_year($day,$month,$year))
+        {
+            $this->day = $day;
+            $this->month = $month;
+            $this->year = $year;
+        }
+        else
+        {
+            $this->day = 01;
+            $this->month = 01;
+            $this->year = 1900;
+        }
+    }
+
+    public function GetDate()
+    {
+        return $this->day.'.'.$this->month.'.'.$this->year;
+    }
+
+    protected static function validate_date($day,$month,$year)
+    {
+        if (self::validate_day($day) and self::validate_month($month) and self::validate_year($year))
+            return true;
+        return false;
+    }
+
+    protected static function validate_day($day)
+    {
+        if(!empty($day) and is_int($day) and ($day > 0 or $day < 32))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    protected static function validate_month($month)
+    {
+        if(!empty($month) and is_int($month) and ($month > 0 or $month < 13))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    protected static function validate_year($year)
+    {
+        $current_year = getdate();
+        if(!empty($year) and is_int($year) and ($year > 1899 or $year < $current_year['year']))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
+
+?>
